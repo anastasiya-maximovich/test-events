21343c725e2a413d2cc4bb35709dd94018a8d3a5
anastasiya025 <anastasiya025.am@gmail.com>
2021-10-12 14:33:54 +0300

    init: start tast event part 2

--- index.js
+++ index.js
@@ -37,4 +37,57 @@ function  countCilinder(){
 
 btnCilinder.addEventListener("click", countCilinder);
 
+ /*
+Task3: Задание:
+Внесите правки в данный сценарий.
+    Добавьте обработчик на клик по кнопке reset. При клике значения, введённые в поля ввода "Операнд 1"
+    и "Операнд 2", должны быть заменены на значения 0
+    Добавьте проверку перед выполнением любой арифметической операции. Если поле ввода пустое, сделайте
+    для этого поля ввода красную рамку и сделайте видимый элемент с ошибкой под этим полем ввода.
+    Для поля ввода input1 при отсутствии значения сделайте видимым span #input1Error для поля ввода
+    input2 сделайте, в случае ошибки, #input2Error видимым.
+    */
+
+let input1 = document.querySelector("#input1");
+let input2 = document.querySelector("#input2");
+let reset = document.querySelector("#reset-btn");
+
+let addBtn = document.querySelector("#add-btn");
+let remBtn = document.querySelector("#rem-btn");
+let mulBtn = document.querySelector("#mul-btn");
+let divBtn = document.querySelector("#div-btn");
+
+
+input1.value = 0;
+input2.value = 0;
+
+reset.addEventListener("click", colculate);
+
+function colculate(btn){
+    if(btn === addBtn){
+        alert(Number(input1.value) + Number(input2.value));
+    }else if(btn === remBtn){
+        alert(input1.value - input2.value);
+    }else if(btn === mulBtn){
+        alert(input1.value * input2.value);
+    }else(btn === divBtn){
+        alert(input1.value / input2.value);
+    }  
+}
+// addBtn.addEventListener("click", function () {
+//     alert(Number(input1.value) + Number(input2.value));
+// });
+
+// remBtn.addEventListener("click", function () {
+//     alert(input1.value - input2.value);
+// });
+
+// mulBtn.addEventListener("click", function () {
+//     alert(input1.value * input2.value);
+// });
+
+// divBtn.addEventListener("click", function () {
+//     alert(input1.value / input2.value);
+// });
+
 
