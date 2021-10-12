f5938f5b1a46a90474e5f2f65f7ad18c2b02ba18
anastasiya025 <anastasiya025.am@gmail.com>
2021-10-12 17:21:06 +0300

    feat: add task 4

--- index.js
+++ index.js
@@ -1,58 +1,45 @@
-/* Task1 --- Задание:
-Сделайте так, чтобы при нажатии на клавишу G фон div#colorOutput становился зеленым.
-при нажатии на клавишу R фон div#colorOutput становился красным, 
-а при нажатии на клавишу B фон div#colorOutput становился синим.
+ /*Task 4 --- Задание:
+Внесите изменения в сценарий. Сделайте чтобы при зажатой клавише Shift и нажатии стрелок на 
+клавиатуре элемент по странице двигался в два раза быстрей, чем если просто нажимать на стрелки.
 */
+let div = document.querySelector("#element");
+let distance = 10;
 
-let div = document.querySelector('#colorOutput');
-
-document.addEventListener('keydown', changeColor);
-
-function changeColor(e){
-    if(e.key === 'g'){
-        div.style.backgroundColor = 'green';
-    }else if(e.key === 'r'){
-        div.style.backgroundColor = 'red';
-    }else if(e.key === 'b'){
-        div.style.backgroundColor = 'blue';
+document.addEventListener("keydown", function (e) {
+    let moveFast = distance;
+    if(e.shiftKey) moveFast *= 2;
+    switch (e.code) {
+        case "ArrowLeft":
+            moveLeft(div, moveFast);
+            break;
+        case "ArrowRight":
+            moveRight(div, moveFast);
+            break;
+        case "ArrowUp":
+            moveUp(div, moveFast);
+            break;
+        case "ArrowDown":
+            moveDown(div, moveFast);
+            break;
     }
+});
+
+function moveUp(element, distance) {
+    let top = getComputedStyle(element).top;
+    element.style.top = parseInt(top) - distance + "px";
 }
 
- /* Task2 -- Задание:
-    Не изменяя расположения элемента script, исправьте ошибку сценария.
-    При загрузке данного сценария на странице должно отобразиться сообщение Hello world
+function moveDown(element, distance) {
+    let top = getComputedStyle(element).top;
+    element.style.top = parseInt(top) + distance + "px";
+}
 
-    script --- document.querySelector("#output").textContent = "Hello world"; --- расположен в head
-    */
+function moveLeft(element, distance) {
+    let left = getComputedStyle(element).left;
+    element.style.left = parseInt(left) - distance + "px";
+}
 
-
-/* Task3 -- Задание:
-    Реализуйте сценарий, который при нажатии на клавиши 1, 2 или 3 на клавиатуре будет добавлять рамку
-    красного цвета на соответствующий div. Если зажаты клавиши Ctrl + A, то красная рамка добавляется сразу
-    для всех div
-*/
-
-let boxes = document.querySelectorAll('.boxes div');
-
-document.addEventListener('keydown', addBorder);
-
-function addBorder(e){
-    console.log(e);
-    if(e.key === '1'){
-        boxes[0].classList.add('divBorder');
-        boxes[1].classList.remove('divBorder');
-        boxes[2].classList.remove('divBorder');
-    }else if(e.key === '2'){
-        boxes[1].classList.toggle('divBorder');
-        boxes[0].classList.remove('divBorder');
-        boxes[2].classList.remove('divBorder');
-    }else if(e.key === '3'){
-        boxes[2].classList.toggle('divBorder');
-        boxes[1].classList.remove('divBorder');
-        boxes[0].classList.remove('divBorder');
-    }else if(e.key === 'A' && e.shiftKey === true){
-        boxes.forEach(box => {
-            box.classList.add('divBorder');
-        })
-    }
+function moveRight(element, distance) {
+    let left = getComputedStyle(element).left;
+    element.style.left = parseInt(left) + distance + "px";
 }
